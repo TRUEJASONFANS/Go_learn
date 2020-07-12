4413db1cb1f44d1ae145ff474d554db23e48dc50
truejasonfans <472408570@qq.com>
2020-07-12 12:51:26 +0800

    add view controller

--- src/photoweb/photoweb.go
+++ src/photoweb/photoweb.go
@@ -2,8 +2,10 @@ package  main
 
 import (
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func uploaderHandler( w http.ResponseWriter, r *http.Request)  {
@@ -16,12 +18,40 @@ func uploaderHandler( w http.ResponseWriter, r *http.Request)  {
 		t.Execute(w, nil)
 		return
 	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+		fileName := h.Filename
+		t, err := os.Create("./uploads/" + fileName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if _,err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view?id=" + fileName, http.StatusFound)
+	}
 
+
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := "./uploads/" + imageId
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
 }
 
 
 func main() {
 	http.HandleFunc("/upload", uploaderHandler)
+	http.HandleFunc("/view", viewHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err.Error())
